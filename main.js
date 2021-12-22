1e18a0b048250998d1ce472aa1f5039b307c003d
1055806 <94760194+1055806@users.noreply.github.com>
2021-12-22 14:12:28 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var vegtables = carrots,broccoli,peas;
+console.log(vegtables);
+
+var fruits = apples,bananna,mango;
+console.log(fruits);
+
+var a = 10;
+var b = 80;
+var c = 20
+var adding = a+b+c;
+console.log(adding);
+
+var squareRoot = - Math.sqrt(24680);
+console.log(squareRoot);
