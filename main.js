df853ead99de11d58314d5de29a1ccbafa66e9cc
1055806 <94760194+1055806@users.noreply.github.com>
2022-04-10 12:44:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-var vegtables = carrots,broccoli,peas;
+var vegtables = "carrots","broccoli","peas";
 console.log(vegtables);
 
-var fruits = apples,bananna,mango;
+var fruits = "apples","bananna","mango";
 console.log(fruits);
 
 var a = 10;
 var b = 80;
 var c = 20
-var adding = a+b+c;
-console.log(adding);
+var add = a+b+c;
+console.log(add);
 
 var squareRoot = - Math.sqrt(24680);
 console.log(squareRoot);
