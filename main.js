727e8d5c4337aaf83ffa9150a23d385d55040e9c
1055806 <94760194+1055806@users.noreply.github.com>
2022-04-10 12:45:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-var vegtables = "carrots","broccoli","peas";
-console.log(vegtables);
-
-var fruits = "apples","bananna","mango";
-console.log(fruits);
-
-var a = 10;
-var b = 80;
-var c = 20
-var add = a+b+c;
-console.log(add);
-
-var squareRoot = - Math.sqrt(24680);
-console.log(squareRoot);
+console.log("My first console project"); var first_name = "Ranbir"; var last_name = "Kapoor"; var full_name = first_name.concat(last_name); console.log(full_name); var a = 10; var b = 5; var add = a + b ; console.log(add); var sub = a - b ; console.log(sub); var multiple = a * b ; console.log(multiple); var divide = a / b ; console.log(divide);
